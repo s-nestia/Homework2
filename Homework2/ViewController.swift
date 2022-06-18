f156d32687113538d80dd120b3df02b0c94f27c5
s-nestia <s.nestia@gmail.com>
2022-06-18 14:39:28 +0300

    Initial Commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Anastasia Liapich on 6/18/22.
+//  Copyright © 2022 Anastasia Liapich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
