2ab817b06b79985b2fbb1935c5a373796308111b
s-nestia <s.nestia@gmail.com>
2022-06-19 14:18:40 +0300

    create traffic light

--- Homework2/ViewController.swift
+++ Homework2/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var stopSignal: UIView!
+    @IBOutlet var waitingSignal: UIView!
+    @IBOutlet var goSignal: UIView!
+    @IBOutlet var lightSwitchButton: UIButton!
+    
+    private let disabledLightAlpha: CGFloat = 0.3
+    private let enabledLightAlpha: CGFloat = 1.0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        stopSignal.layer.cornerRadius = stopSignal.frame.width / 2
+        waitingSignal.layer.cornerRadius = waitingSignal.frame.width / 2
+        goSignal.layer.cornerRadius = goSignal.frame.width / 2
+        
+        lightSwitchButton.layer.cornerRadius = 10
+        
+        stopSignal.alpha = disabledLightAlpha
+        waitingSignal.alpha = disabledLightAlpha
+        goSignal.alpha = disabledLightAlpha
     }
 
-
+    @IBAction func switchLight(_ sender: Any) {
+        if stopSignal.alpha == enabledLightAlpha {
+            waitingSignal.alpha = enabledLightAlpha
+            stopSignal.alpha = disabledLightAlpha
+        } else if waitingSignal.alpha == enabledLightAlpha {
+            goSignal.alpha = enabledLightAlpha
+            waitingSignal.alpha = disabledLightAlpha
+        } else if goSignal.alpha == enabledLightAlpha {
+            stopSignal.alpha = enabledLightAlpha
+            goSignal.alpha = disabledLightAlpha
+        } else {
+            lightSwitchButton.setTitle("NEXT", for: .normal)
+            stopSignal.alpha = enabledLightAlpha
+        }
+    }
 }
-
